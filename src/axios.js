e69c6812333213d3cc210c1ae10fb21e1ecc8f66
moturi07 <moturigeorge@gmail.com>
2021-08-07 01:43:09 +0300

    modules update

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL='http://moturi-001-site1.ftempurl.com/api/';
+axios.defaults.baseURL='moturi-001-site1.ftempurl.com/api/';
 
 axios.defaults.headers.common['Authorization']='Bearer'+localStorage.getItem('token');
