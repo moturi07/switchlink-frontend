860eeb4aa4319e7ccbb7932567f616bc79a5a46f
moturi07 <moturigeorge@gmail.com>
2021-08-07 07:41:30 +0300

    final

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL='moturi-001-site1.ftempurl.com/api/';
+axios.defaults.baseURL='https://moturi-atm.somee.com/api/';
 
 axios.defaults.headers.common['Authorization']='Bearer'+localStorage.getItem('token');
