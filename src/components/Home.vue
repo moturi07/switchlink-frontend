eeee215dfbd389d742eceba7b0f4bd9900845f69
moturi07 <36444335+moturi07@users.noreply.github.com>
2021-08-07 07:58:46 +0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-	<h2 class="p-2">Welcome to the Frontend part of the project</h2>
+	<h2 class="mt-5 p-2 text-center">Welcome to the Frontend part of the project</h2>
 <div class="text-center p-5 m-auto" v-if="loggedIn">
     <h4>Welcome Back {{user.email}}</h4>
     <a href="dashboard" class="btn p-3 btn-primary">View Dashboard</a>
